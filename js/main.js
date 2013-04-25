6a82a43f2dbe16922ec4a8f48f39ebdd29a01e6b
Gleb Makarov <gleb@efley.ee>
2013-04-25 17:35:59 +0300

    13

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-
-var aligner = '#diary';
+var LATEST = '';
+var aligner = '#treenerpakkumised';
 var plus = 0;
 var last = "";
 var arr = new Array();
@@ -7,6 +7,8 @@ var offset = jQuery('.topbar').height();
 var me = "";
 var bbar = "";
 var wind = "";
+
+var toBuy = new Array();
 
 
 
@@ -23,15 +25,16 @@ function resizeby(_this, _plus){
 	var height = $(_this).height();
 	var newheight = Number( height + _plus );
 	
+	LATEST = _this;
 	
 	offset = jQuery('.topbar').height();
 	me = jQuery(_this + ' .me').height();
 	bbar = jQuery('.bottombar').height();
 	wind = jQuery(window).height();
-	
+
 	
 	var toscrollheight = Number( wind - ( offset + me + bbar ) );
-	console.log(newheight , toscrollheight);
+	//console.log(newheight , toscrollheight);
 	
 	jQuery('.centered').css('top', offset + 'px');
 	jQuery('.page-wrap').css('height', wind + 'px');
@@ -44,7 +47,7 @@ function resizeby(_this, _plus){
 }
 function reposition(){
 		
-	jQuery('.loading').hide();
+	//jQuery('.loading').hide();
 	for(var i=1; i < arr.length; i++){
 		jQuery('#'+arr[i]).removeClass('open').removeClass('hide');
 	}	
@@ -59,7 +62,7 @@ function reposition(){
 	$(document).ready(function (){
 		
 		setTimeout( function(){
-			resizeby('#diary', 105);
+			resizeby('#treenerpakkumised', 105);
 			$('body').css('opacity','1');
 			offset = jQuery('.topbar').height();
 		}, 200);
@@ -75,8 +78,14 @@ function reposition(){
 			arr = last.split('#');
 			
 			for(var i=1; i < arr.length; i++){
-				$('#'+arr[i]).addClass('hide');
+				//$('#'+arr[i]).addClass('hide');
 			}	
+			
+			$(LATEST).addClass('hide');
+			
+			$('#menu').removeClass('active');
+			$('#themenu').removeClass('show');
+			$('.topbar, .bottombar, '+aligner).removeClass('showmenu');
 		}
 		
 		
@@ -96,23 +105,17 @@ function reposition(){
 			}
 		});
 		
-		/* $('#themenu').bind('swipe', function(){
-				$('#menu').removeClass('active');
-				$('#themenu').removeClass('show');
-				$('.topbar, .bottombar, '+aligner).removeClass('showmenu');
-        }); */
-		
 		$('body').touchwipe({
 			 wipeLeft: function() { 
 					$('#menu').removeClass('active');
 					$('#themenu').removeClass('show');
 					$('.topbar, .bottombar, '+aligner).removeClass('showmenu');
 			 },
-			 wipeRight: function() { alert("right"); },		 
+			 //wipeRight: function() { alert("right"); },		 
 			//wipeUp: function() { alert("up"); },
 			//wipeDown: function() { alert("down"); },
-			 min_move_x: 20,
-			 min_move_y: 20,
+			 min_move_x: 15,
+			 min_move_y: 15,
 			 preventDefaultEvents: false
 		});
 
@@ -122,11 +125,11 @@ function reposition(){
 			$(this).addClass('active');
 			
 			hideallexcept('#frontpage');	
-			$('#frontpage').addClass('open').removeClass('hide');
+			$('#frontpage').addClass('open');
 			
 			$('.bottombar, .topbar').removeClass('menuin');
 			
-			$('.loading').show();
+			//$('.loading').show();
 			
 			resizeby('#frontpage', 105);
 			
@@ -137,16 +140,16 @@ function reposition(){
 		});
 		
 		
-		$('.fb').click(function(e){
+		$('.fb, .homebtn').click(function(e){
 			e.preventDefault();
 			$(this).addClass('active');
 			
 			hideallexcept('#index');
-			$('#index').addClass('open').removeClass('hide');
+			$('#index').addClass('open');
 			
 			$('.bottombar, .topbar').addClass('menuin');
 			
-			$('.loading').show();
+			//$('.loading').show();
 			
 			resizeby('#index', 105);
 			
@@ -161,10 +164,10 @@ function reposition(){
 			$(this).addClass('active');
 			
 			hideallexcept('#login');
-			$('#login').addClass('open').removeClass('hide');
+			$('#login').addClass('open');
 			
 			
-			$('.loading').show();
+			//$('.loading').show();
 			
 			resizeby('#login', 105);
 			
@@ -173,14 +176,191 @@ function reposition(){
 			}, 300);
 			
 		});
+		
+			
+		$('.diarybtn').click(function(e){
+			e.preventDefault();
+			$(this).addClass('active');
+			
+			hideallexcept('#diary');
+			$('#diary').addClass('open');
+			
+			
+			//$('.loading').show();
+			
+			resizeby('#diary', 105);
+			
+			setTimeout(function(){
+				reposition();
+			}, 300);
+		});
 			
 			
 			
+		
+		$('.pakkumisedbtn').click(function(e){
+			e.preventDefault();
+			$(this).addClass('active');
 			
+			hideallexcept('#pakkumised');
+			$('#pakkumised').addClass('open');
+			
+			
+			//$('.loading').show();
+			
+			resizeby('#pakkumised', 105);
+			
+			setTimeout(function(){
+				reposition();
+			}, 300);
+		});
+			
+			
+		
+		$('.soodustusedbtn').click(function(e){
+			e.preventDefault();
+			$(this).addClass('active');
+			
+			hideallexcept('#soodustused');
+			$('#soodustused').addClass('open');
+			
+			
+			//$('.loading').show();
+			
+			resizeby('#soodustused', 105);
+			
+			setTimeout(function(){
+				reposition();
+			}, 300);
+		});
+			
+			
+		
+		$('.lisandidbtn').click(function(e){
+			e.preventDefault();
+			$(this).addClass('active');
+			
+			hideallexcept('#lisandid');
+			$('#lisandid').addClass('open');
+			
+			
+			//$('.loading').show();
+			
+			resizeby('#lisandid', 105);
+			
+			setTimeout(function(){
+				reposition();
+			}, 300);
+		});
+
+		
+			
+		
+		$('.treenerpakkumisedbtn').click(function(e){
+			e.preventDefault();
+			$(this).addClass('active');
+			
+			hideallexcept('#treenerpakkumised');
+			$('#treenerpakkumised').addClass('open');
+			
+			
+			//$('.loading').show();
+			
+			resizeby('#treenerpakkumised', 105);
+			
+			setTimeout(function(){
+				reposition();
+			}, 300);
+		});
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		$('.selectbtn img').click(function(e){
+			//e.preventDefault();
+			var id = $(this).parent().parent().data('id');
+			if(!$(this).parent().parent().hasClass('selected')){
+				$(this).parent().parent().addClass('selected');					
+				toBuy.push(id);			
+			}else{
+				$(this).parent().parent().removeClass('selected');	
+				while (toBuy.indexOf( id ) !== -1) {
+					toBuy.splice(toBuy.indexOf( id ), 1);
+				}
+			}			
+			if(toBuy.length >= 1){
+				$('.buybtn').addClass('slideIn');
+			}else{
+				$('.buybtn').removeClass('slideIn');
+			}
+			//console.log(toBuy.length);
+		});
+		
+		
+		
+		
+		
+		$('.buybtn').click(function(e){
+			//e.preventDefault();
+			
+			$('#buyoverlay').addClass('prepare').addClass('scale');
+			setTimeout(function(){
+				$('#buyoverlay').addClass('scaleIn');
+			}, 500);
+			
+			var here = $('#buyoverlay .checkout');
+			
+			here.html('');
+			
+			for(var i = 0; i < toBuy.length; i++){
+				
+				var id = toBuy[i];
+				
+				var name = $('#treenerpakkumised .box33[data-id='+ id +']').children('h4.name').text();
+				var price = $('#treenerpakkumised .box33[data-id='+ id +']').children('h4.price').text();
+				
+				var html = '<div class="inbasket"></div>';
+				
+				here.append( name + ' ' );
+				
+			}
+			
+		});
+		
+		
+		$('.detailsbtn').click(function(e){
+			//e.preventDefault();
+			
+			$('#overlay').addClass('prepare').addClass('scale');
+			setTimeout(function(){
+				$('#overlay').addClass('scaleIn');
+			}, 500);
+		});
+		
+		
+		$('.backbtn').click(function(e){
+			e.preventDefault();
+			
+			$(this).parent().removeClass('scaleIn');
+			
+			var _this = $(this);
+			
+			setTimeout(function(){
+				_this.parent().removeClass('scale').removeClass('prepare');
+			}, 400);
+		});
+		
 			
 		/* MAKE HOVER */
+		/* FOR STATIC ELEMENTS ONLY, NOT WORKING ON SCROLLABLE!! */
 		
-		 $('.touchhover').on('touchstart', function(){
+		 $('.touchhover').on('touchstart', function(e){
 			//e.preventDefault();
 			$(this).addClass('hover');
 		}).on('touchend', function(e){
