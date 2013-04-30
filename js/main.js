c904bf25b65b6d90825ec3994549db6915465bef
Gleb Makarov <gleb@efley.ee>
2013-04-30 17:40:01 +0300

    25

--- js/main.js
+++ js/main.js
@@ -492,30 +492,7 @@ $(window).load(function () {
 				
 			});
 			
-			
-			 $(".touchslider").touchSlider({
-				container: this,
-				duration: 350, // the speed of the sliding animation in milliseconds
-				delay: 3000, // initial auto-scrolling delay for each loop
-				margin: 5, // borders size. The margin is set in pixels.
-				mouseTouch: true,
-				namespace: "touchslider",
-				next: ".touchslider-next", // jQuery object for the elements to which a "scroll forwards" action should be bound.
-				currentClass: "touchslider-nav-item-current", // class name for current pagination item.
-				prev: ".touchslider-prev", // jQuery object for the elements to which a "scroll backwards" action should be bound.
-				autoplay: false // whether to move from image to image automatically
-			 });
-			
-			var t=0; // the height of the highest element (after the function runs)
-			$(".touchslider-item").each(function () {
-				$this = $(this);
-				if ( $this.outerHeight() > t ) {
-					t=$this.outerHeight();
-				}
-			});
-		
-			$('.touchslider-viewport, .touchslider-viewport div').height( t );
-			
+
 			
 			
 		} /* bindEvents */
